8ed1e63a3e955f9aa429521a42de295a344852f4
Violennx <renggaking122@gmail.com>
2025-05-13 13:25:23 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+// Konfigurasi koneksi database
+const db = mysql.createConnection({
+    host: "demo1-violen.l.aivencloud.com",       // Host dari database
+    user: "avnadmin",            // Username Anda
+    password: "AVNS_4-MOSOik9vfYQUM9MxJ", // Ganti dengan password yang Anda masukkan di MySQL Workbench
+    database: "qrcodeku", // Nama database yang Anda buat
+    port: 19997               // Port default MySQL
+});
+
+db.connect(err => {
+    if (err) {
+        console.error("Gagal terhubung ke database: " + err.message);
+        process.exit();
+    }
+    console.log("Terhubung ke database!");
+});
+
+module.exports = db; // Ekspor koneksi untuk digunakan di server.js
+
